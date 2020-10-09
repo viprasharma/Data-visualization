ba5a5fc7ab707a5f2906d1198432863cd3b8825b
viprasharma <54271021+viprasharma@users.noreply.github.com>
2020-10-09 14:23:05 +0530

    Update plot.py

--- Teacher refrence/plot.py
+++ Teacher refrence/plot.py
@@ -1,9 +1,1 @@
-import pandas as pd
-
-import plotly.express as px
-
-df = pd.read_csv("line_chart.csv")
-
-fig = px.line(df, x="Year", y="Per capita income", color="Country", title='Per Capita Income')
-
-fig.show()
+//line plot by line chart
