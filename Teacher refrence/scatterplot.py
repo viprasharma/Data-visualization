67e5615f3faaae34b7ced0c9cda8842c86bce600
viprasharma <54271021+viprasharma@users.noreply.github.com>
2020-10-09 14:24:29 +0530

    Update scatterplot.py

--- Teacher refrence/scatterplot.py
+++ Teacher refrence/scatterplot.py
@@ -1,8 +1,1 @@
-import pandas as pd
-import plotly.express as px
-
-df = pd.read_csv("data.csv")
-fig = px.scatter(df, x="Population", y="Per capita",
-	          size="Percentage",color="Country",
-                   size_max=60)
-fig.show()
+//scatter representation
